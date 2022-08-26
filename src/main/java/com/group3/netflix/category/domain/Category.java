8f59871dccb96509048ae0b0b9b0c07030d01bcb
Jordi Gómez Hidalgo <31970428+gnugomez@users.noreply.github.com>
2022-08-26 12:18:42 +0200

    feat: create category entity (#13)
    * feat: add category entity
    
    * chore: update category to match lombok
    
    * feat: add category repository
    
    * feat: delete gitkeep
    
    * feat: delete gitkeep

---
+++ src/main/java/com/group3/netflix/category/domain/Category.java
@@ -0,0 +1,24 @@
+package com.group3.netflix.category.domain;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "name")
+    @JsonProperty(value = "name", required = true)
+    private String name;
+}
